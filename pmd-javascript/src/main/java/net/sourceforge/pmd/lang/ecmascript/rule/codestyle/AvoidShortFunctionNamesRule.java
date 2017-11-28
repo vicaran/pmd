858d92aa038937ea62ed3b51dda5b6ac201e7d60
Andrea <vicara@usi.ch>
2017-11-28 20:01:28 +0100

    fix on name rule for functions

--- pmd-javascript/src/main/java/net/sourceforge/pmd/lang/ecmascript/rule/codestyle/AvoidShortFunctionNamesRule.java
+++ pmd-javascript/src/main/java/net/sourceforge/pmd/lang/ecmascript/rule/codestyle/AvoidShortFunctionNamesRule.java
@@ -5,17 +5,13 @@
 package net.sourceforge.pmd.lang.ecmascript.rule.codestyle;
 
 import net.sourceforge.pmd.lang.ecmascript.ast.ASTFunctionNode;
-import net.sourceforge.pmd.lang.ecmascript.ast.ASTName;
 import net.sourceforge.pmd.lang.ecmascript.rule.AbstractEcmascriptRule;
 
 public class AvoidShortFunctionNamesRule extends AbstractEcmascriptRule {
 
     @Override
     public Object visit(ASTFunctionNode functionNode, Object data) {
-        if (functionNode.findDescendantsOfType(ASTName.class).size() > 0
-                && functionNode.findDescendantsOfType(ASTName.class).get(0).getIdentifier() != null
-                && functionNode.findDescendantsOfType(ASTName.class).get(0).getIdentifier().length() > 0
-                && functionNode.findDescendantsOfType(ASTName.class).get(0).getIdentifier().length() < 3) {
+        if (functionNode.getFunctionName() != null && functionNode.getFunctionName().getImage().length() < 3) {
             addViolation(data, functionNode);
         }
         return super.visit(functionNode, data);
